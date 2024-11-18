2376885618fb23723f6da9c1d446ca98dccfa495
Aaron Yuan <ayuan1114@gmail.com>
2024-11-18 11:40:48 -0600

    improve classifier

--- backend/classifier.py
+++ backend/classifier.py
@@ -7,4 +7,8 @@ class TicketClassifier:
     
     def categorize(self, desc):
         result = self.classifier(desc, candidate_labels=self.categories)
-        return result['labels'][0]
+        print(result)
+        if result['scores'][0] > 2 / len(self.categories):
+            return result['labels'][0]
+        else:
+            return 'Other'
